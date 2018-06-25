044f4ea4ae833521c06788dd440c3f1b9a50014f
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 02:23:31 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,24 @@
+require 'json'
+file = File.read('spec/fixtures/contestants.json')
+data_hash = JSON.parse(file)
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winners = []
+  data.each do |seasons, info|
+    if seasons = season
+     info.each do |value|
+       if value["status"] == "Winner"
+         a = value["name"].split(" ")
+         a.pop
+         winners.push(a)
+       end
+     end
+  end
+  print winners.flatten(1)
 end
+
+get_first_name_of_season_winner(data, "season 10")
 
 def get_contestant_name(data, occupation)
   # code here
@@ -17,3 +35,6 @@ end
 def get_average_age_for_season(data, season)
   # code here
 end
+
+
+#get_first_name_of_season_winner()
